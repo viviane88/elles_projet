ed88effaae140934cb8c490a1b9f46be3e7fdd8c
vi <fish.design.co@gmail.com>
2020-11-16 12:48:13 +0100

    CRUD job

--- src/Controller/Administrateur/JobAdminController.php
+++ src/Controller/Administrateur/JobAdminController.php
@@ -2,19 +2,103 @@
 
 namespace App\Controller\Administrateur;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Jobs;
+use App\Form\JobType;
+use App\Repository\JobsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class JobAdminController extends AbstractController
 {
     /**
      * @Route("/job/admin", name="job_admin")
      */
-    public function index(): Response
+    public function index(JobsRepository $jobsRepository): Response
     {
+        $jobs = $jobsRepository->findAll();
+
         return $this->render('Administrateur/jobAdmin.html.twig', [
-            'controller_name' => 'JobAdminController',
+            'jobs' => $jobs,
         ]);
+    }
+    /**
+     * @Route("/job/admin/create", name="job_create")
+     */
+    public function createJob(Request $request)
+    {
+        $jobs = new Jobs();
+        
+        $form = $this->createForm(JobType::class, $jobs);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($jobs);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Le nouvel emploi a bien été ajoutée'
+            );
+            return $this->redirectToRoute('job_admin');
+
+        }
+        return $this->render('Administrateur/jobAdminForm.html.twig', [
+            'formulaireJobs' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/admin/job/update-{id}", name="job_update")
+     */
+    public function updateJob(JobsRepository $jobsRepository, $id, Request $request)
+    {
+        $jobs = $jobsRepository->find($id);
+
+
+        $form = $this->createForm(jobType::class, $jobs);
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()){
+            
+           
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($jobs);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Le nouvel emploi a bien été modifié'
+            );
+
+            return $this->redirectToRoute('job_admin');
+        }
+
+        return $this->render('Administrateur/jobAdminForm.html.twig', [
+            'formulaireJobs' => $form->createView(),
+        ]);
+
+    }
+
+    /**
+     * @Route("/admin/job/delete-{id}", name="job_delete")
+     */
+    public function deletejob(JobsRepository $jobsRepository, $id)
+    {
+        $jobs = $jobsRepository->find($id);
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($jobs);
+        $manager->flush();
+
+        $this->addFlash(
+            'danger',
+            'Le nouvel emploi a bien été supprimée'
+        );
+        
+
+        return $this->redirectToRoute('job_admin');
     }
 }
---
+++ src/Form/JobType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Jobs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('Title', TextType::class,[
+            'required'=> true,
+            'label' => 'Titre de l\'annonce',
+            'attr' => [
+                'placeholder' =>'Poste proposé'
+            ]
+        ])            
+        ->add('Company', TextType::class, [
+            'required'=> true,
+            'label' => 'Entreprise',
+            'attr' => [
+                'placeholder' => 'Le nom de l\'entreprise'
+            ]
+
+        ])            
+        ->add('location', TextType::class, [
+            'required'=> true,
+            'label' => 'Adresse',
+            'attr' => [
+                'placeholder' => 'adresse de l\'entreprise'
+            ]
+
+        ])            
+        ->add('description', TextType::class, [
+            'required'=> true,
+            'label' => 'Description de l\'annonce',
+            'attr' => [
+                'placeholder' => 'décrire le poste proposé'
+            ]
+
+        ])
+        ->add('url', UrlType::class, [
+                'required'=> true,
+                'attr' => [
+                    'placeholder' => 'URL, de l\'entreprise'
+                ]
+
+            ])             
+        ->add('createdAt', DateType::class, [
+                'label' => 'Création de l\'annonce',
+                'required'=> true,
+                'attr' => [
+                    'placeholder' => 'date de publication'
+                ]
+
+            ])
+        ->add('updated_at', DateType::class, [
+                'label' => 'Ecpiration de l\'annonce',
+                'attr' => [
+                    'placeholder' => 'date d\'expiration'
+                ]
+
+            ])
+        /* ->add('uploads', FileType::class, [
+                'mapped'=> false,
+                'label' => 'logo 1',
+                'attr' => [
+                    'placeholder' => 'ex: logo.jpg'
+                ]
+            ])  */           
+        ->add('Sauvegarder', SubmitType::class, [
+                'label' => 'Valider'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Jobs::class,
+        ]);
+    }
+}
