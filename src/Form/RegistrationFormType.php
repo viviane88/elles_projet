12b935fa18a3b956eac3a25ae0c1e852c87e21ed
vi <fish.design.co@gmail.com>
2020-11-16 14:27:25 +0100

    security password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,8 +9,10 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Rollerworks\Component\PasswordStrength\Validator\Constraints\PasswordStrength;
 
 class RegistrationFormType extends AbstractType
 {
@@ -38,22 +40,30 @@ class RegistrationFormType extends AbstractType
             ],
             'label' => 'Merci d\'accepter les conditions générales d\'utilisation'
         ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Votre mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+        ->add('plainPassword', PasswordType::class, [
+            // instead of being set onto the object directly,
+            // this is read and encoded in the controller
+            'mapped' => false,
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Votre mot de passe',
+                ]),
+                new PasswordStrength([
+                    // longueur mini
+                    'minLength' => 8,
+                    'tooShortMessage' => 'Le mot de passe doit contenir au moins 8 caractères, 
+                    lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial',
+                    
+                    // force mini
+                    'minStrength' => 4,
+                    'message' => 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'
+                ])
+            ],
+            'attr'=> [
+                'placeholder' => 'Votre mot de passe'
+            ],
+            'label' => 'Mot de passe'
+        ])
         ;
     }
 
